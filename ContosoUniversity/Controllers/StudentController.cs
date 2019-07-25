8133a03614b7e92bb410f20d9ecd299d807bfbfb
dnkato <diane.n.kato@gmail.com>
2019-07-25 10:23:11 -0700

    Added Entity Framework Connection Resiliency / Retry Logic

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
 using PagedList;
+using System.Data.Entity.Infrastructure;
 
 namespace ContosoUniversity.Controllers
 {
@@ -104,7 +105,7 @@ namespace ContosoUniversity.Controllers
                 }
 
             }
-            catch (DataException /*dex*/)
+            catch (RetryLimitExceededException /*dex*/)
             {
                 //Log the error (uncomment dex variable name and add a line here to write a log.
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
@@ -149,7 +150,7 @@ namespace ContosoUniversity.Controllers
 
                     return RedirectToAction("Index");
                 }
-                catch (DataException /* dex */)
+                catch (RetryLimitExceededException /* dex */)
                 {
                     //Log the error (uncomment dex variable name and add a line here to write a log.
                     ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
@@ -194,7 +195,7 @@ namespace ContosoUniversity.Controllers
 
                 db.SaveChanges();
             }
-            catch (DataException /*dex*/)
+            catch (RetryLimitExceededException/*dex*/)
             {
                 //Log the error (uncomment dex variable name and add a line here to write a log.
                 return RedirectToAction("Delete", new { id = id, saveChangesError = true });
