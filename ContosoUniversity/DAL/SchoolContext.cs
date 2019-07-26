f557174c2ed3ccf3d949fa7a3f02d962af81b898
dnkato <diane.n.kato@gmail.com>
2019-07-26 11:35:47 -0700

    Creating a more comples data model

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -17,15 +17,27 @@ namespace ContosoUniversity.DAL
         {
         }
 
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
-        // prevents table names from being pluralized. If you didn't do this, the generated tables in the database would 
-        // be named Students, Courses, and Enrollments. Instead, the table names will be Student, Course, and Enrollment. 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            // prevents table names from being pluralized. If you didn't do this, the generated tables in the database would 
+            // be named Students, Courses, and Enrollments. Instead, the table names will be Student, Course, and Enrollment. 
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // For the many-to-many relationship between the Instructor and Course entities, the code specifies the table and 
+            // column names for the join table. Code First can configure the many-to-many relationship for you without this code, 
+            // but if you don't call it, you will get default names such as InstructorInstructorID for the InstructorID column.
+            modelBuilder.Entity<Course>()
+                 .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                 .Map(t => t.MapLeftKey("CourseID")
+                     .MapRightKey("InstructorID")
+                     .ToTable("CourseInstructor"));
         }
     }
 }
